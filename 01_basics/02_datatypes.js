b023f6814ec2b77e973d9e44dfdc3007335327ab
anurag2909 <77746382+anurag2909@users.noreply.github.com>
2023-09-08 08:15:53 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all JS code as a newer version. We can even avoid this line. Js engine will defaultly treat everything as newer version.
+
+alert (3+3) // Will give error here as we are using nodejs here not browser
+
+// Number => 2 to power 53 (range)
+// Bigint => if the  number is greater that Number datatype capacity (specially in stocks,trading)
+// String => for a string value (use in "..."/'...');
+// Boolean => true/false
+// Null => standalone value/representation of an empty value
+// Undefined => value has not been assigned as of now
+// Symbol => mainly used in react to find uniqueness in different components
+// Object 
+
+console.log(typeof "Anurag"); // string
+console.log(typeof age); // number
+console.log(typeof null) // object (to be noted)
+console.log(typeof undefined) // undefined
